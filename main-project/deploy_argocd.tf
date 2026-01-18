8df606f6af7606fd547d77796388750038efe274
plavez <plavez@gmail.com>
2026-01-18 20:12:03 +0000

    GitOps demo: scale myapp1 to 3 replicas

--- main-project/deploy_argocd.tf
+++ main-project/deploy_argocd.tf
@@ -17,22 +17,22 @@ module "argocd_prod" {
 # ## Second step deploying
 # ## Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_dev]
 # ## Root для DEV
-# module "argocd_dev_root" {
-#   source          = "./terraform_argocd_root_eks"
-#   kubeconfig_path = pathexpand("~/.kube/dev-k3s.yaml")
+module "argocd_dev_root" {
+  source          = "./terraform_argocd_root_eks"
+  kubeconfig_path = pathexpand("~/.kube/dev-k3s.yaml")
 
-#   git_source_path           = "dev/applications"
-#   git_source_repoURL        = "https://github.com/plavez/argocd.git"
-#   git_source_targetRevision = "main"
-# }
+  git_source_path           = "dev/applications"
+  git_source_repoURL        = "https://github.com/plavez/argocd.git"
+  git_source_targetRevision = "main"
+}
 
 # ### Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_prod]
 # ### Root для PROD
-# module "argocd_prod_root" {
-#   source          = "./terraform_argocd_root_eks"
-#   kubeconfig_path = pathexpand("~/.kube/prod-k3s.yaml")
+module "argocd_prod_root" {
+  source          = "./terraform_argocd_root_eks"
+  kubeconfig_path = pathexpand("~/.kube/prod-k3s.yaml")
 
-#   git_source_path           = "prod/applications"
-#   git_source_repoURL        = "https://github.com/plavez/argocd.git"
-#   git_source_targetRevision = "main"
-# }
+  git_source_path           = "prod/applications"
+  git_source_repoURL        = "https://github.com/plavez/argocd.git"
+  git_source_targetRevision = "main"
+}
